163e997162517a4a3390c51d1d1cdb0b1c4f891c
o2fan9 <148854890+o2fan9@users.noreply.github.com>
2023-12-25 23:50:29 +0800

    新增canva

--- ChineseDB.vue
+++ ChineseDB.vue
@@ -132,6 +132,14 @@ date: new Date()})
 <template>
   <v-container>
     <v-select label="請選擇" v-model="state.choice" :items="units"> </v-select>
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF3yD7LsRI&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+F.Y. 設計的 <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF3yD7LsRI&#x2F;view?utm_content=DAF3yD7LsRI&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">文學常識</a>
     <div v-for="(exam, index) in state.exams" :key="index">
       <v-text-field
         v-if="exam.type == 'blank'"
